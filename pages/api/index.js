c901875b7d2b54f5a13fd259df3ad975f2116abd
Prasang Charitra <150513432+bulkhead1tech@users.noreply.github.com>
2025-05-19 15:19:08 +0530

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,1 +1,17 @@
+import { connectdb } from "@/utils/mongoose.utils"
 
+const handler =async (req, res)=>{
+    try {
+        await connectdb();
+   
+        
+        
+    } catch (error) {
+        console.log(error)
+    }
+        
+
+
+
+}
+export default handler;
