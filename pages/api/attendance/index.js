df4ee1fb5d603bfd8d4f94fb49989a422256dda6
Prasang Charitra <150513432+bulkhead1tech@users.noreply.github.com>
2025-05-19 15:21:30 +0530

    Update index.js

--- pages/api/attendance/index.js
+++ pages/api/attendance/index.js
@@ -3,7 +3,7 @@ import { connectdb } from "@/utils/mongoose.utils"
 
 const handler =async (req, res)=>{
     try {
-        await connectdb();
+    
         const {user, date} = await req.body.formData;
         const att = await Attendance.findOne({user:user, date:date})
 
