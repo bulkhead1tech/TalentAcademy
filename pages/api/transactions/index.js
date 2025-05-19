154cf865544aa2eb6ee392537888f36718de9d55
Prasang Charitra <150513432+bulkhead1tech@users.noreply.github.com>
2025-05-19 15:25:43 +0530

    Update index.js

--- pages/api/transactions/index.js
+++ pages/api/transactions/index.js
@@ -1,8 +1,6 @@
 import Transaction from "@/models/Transaction.model"
-import { connectdb } from "@/utils/mongoose.utils";
 const handler =async (req, res)=>{
     try {
-        await connectdb();
         const data= await Transaction.find();
         res.send(data)
     } catch (error) {
