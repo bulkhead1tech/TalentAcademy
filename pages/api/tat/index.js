60f656428f4ba59982202cc905ff5eee901ef91f
Prasang Charitra <150513432+bulkhead1tech@users.noreply.github.com>
2025-05-19 15:25:24 +0530

    Update index.js

--- pages/api/tat/index.js
+++ pages/api/tat/index.js
@@ -1,8 +1,7 @@
-import { connectdb } from "@/utils/mongoose.utils"
 import TAT from "@/models/tat.model";
 const handler =async (req, res)=>{
     try {
-        await connectdb();
+        
         const number = req.body.count;
         const num = parseInt(number);
         const data = await TAT.aggregate([{ $sample: { size: num } }]).exec();
